cd670c9dd7a2f94aebc5df1ba5be8055d327d243
Soraya Sakonwittayanon <soraya.sakonwittayanon@gmail.com>
2017-02-19 17:01:14 +0700

    try 3 homepage

---
+++ test/home-page3/assets/js/autoplay.js
@@ -0,0 +1,38 @@
+$(function(){
+  var fadeShow = $(".background").fadeShow({
+    correctRatio: true,
+    shuffle: true,
+    speed: 10000,
+    images: ['./assets/images/maxresdefault.jpg',
+         './assets/images/rain.jpg',
+         './assets/images/rain-umbrella.jpg']
+  });
+});
+
+$('a[href^="#result"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
+
+jQuery(document).ready(function() {
+    var offset = 220;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+
+    jQuery('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
